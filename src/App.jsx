d868fd1c1746a9e17083a2a722a578386413e073
brksnr <berksener04@gmail.com>
2025-01-13 22:56:54 +0300

    arama çubuğu eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,15 @@ function App() {
         </div>
       </div>
 
-      <div className='bg-[url("images/1.jpg")] w-screen h-screen'>
-        <Button></Button>
+      <div className='bg-[url("images/1.jpg")] w-screen h-screen flex justify-center items-center gap-5'>
+        <div class="flex items-center w-full max-w-md">
+          <input
+            type="text"
+            placeholder="Film, Oyuncu, Yönetmen, Tür.."
+            class="w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
+            />
+          </div>
+        <Button>SEARCH</Button>
       </div>
       
     </>
